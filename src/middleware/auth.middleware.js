9aae33947bc925df3ec741fff972447370495d9f
Sergio1977 <sergio_cofla@yahoo.com.ar>
2025-11-26 21:03:47 -0300

    arreglos

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -11,8 +11,7 @@ export const authentication = (req, res, next) => {
     const token = req.headers['authorization'].split(" ")[1];
     console.log(token)
 
-    if (!token) return res.sendStatus(401);
-
+    if (!token) return res.status(401).json({ message: 'Token perdido sin autorization' });
     jwt.verify(token, secret_key, (err) => {
         if (err) return res.sendStatus(403);
         next();
